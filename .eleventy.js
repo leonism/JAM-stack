929ebc860813cf8f71f983e7e6dbb178fa6dd5f9
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-11 16:21:29 +0700

    second commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,20 @@
+// adding filter for the timestamp on the blog section
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).
+      toLocaleString(DateTime.DATE_MED);
+    }
+  )
   return {
+    passthroughFileCopy: true,
     dir: {
       input: "src",
       output: "public"
+      
     }
   }
 }
